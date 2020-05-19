9295d8e43e57c50096d868db825fea9003ca7518
nanakusa27 <masakiw27@yahoo.co.jp>
2020-05-19 17:27:20 +0900

    Add:配列並べ替え、初回コミット

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -3,7 +3,13 @@ $array = explode(',', $_GET['array']);
 
 // 修正はここから
 for ($i = 0; $i < count($array); $i++) {
-
+  for ($n = $i+1; $n < count($array); $n++) {
+    if ($array[$i] > $array[$n]) {
+      $ex = $array[$i];
+      $array[$i] = $array[$n];
+      $array[$n] = $ex;
+    }
+  }
 }
 // 修正はここまで
 
