462f82fd668b1e353bb2d1f904c7f01cdf5151d4
nanakusa27 <masakiw27@yahoo.co.jp>
2020-05-19 21:03:30 +0900

    Improve:for文 ($length-1)

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -4,7 +4,7 @@ $array = explode(',', $_GET['array']);
 // 修正はここから
 $length = count($array);
 
-for ($i = 0; $i < $length; $i++) {
+for ($i = 0; $i < $length - 1; $i++) {
   for ($n = $i + 1; $n < $length; $n++) {
     if ($array[$i] > $array[$n]) {
       $tmp = $array[$i];
