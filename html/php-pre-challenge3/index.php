e6d131bd2f955bf9259ea2d9e6ab3b2d199f1c38
nanakusa27 <masakiw27@yahoo.co.jp>
2020-05-20 11:05:49 +0900

    Add:DB接続、値取り出し

--- html/php-pre-challenge3/index.php
+++ html/php-pre-challenge3/index.php
@@ -4,3 +4,26 @@ $limit = $_GET['target'];
 $dsn = 'mysql:dbname=test;host=mysql';
 $dbuser = 'test';
 $dbpassword = 'test';
+
+// DB接続
+try {
+  $db = new PDO($dsn, $dbuser, $dbpassword);
+} catch (PDOException $e) {
+  echo 'DB接続エラー：' . $e->getMessage();
+}
+
+$numbers = $db->query('SELECT value FROM prechallenge3');
+
+$i = 0;
+while($number = $numbers->fetch()) {
+  // print($number['value']);
+  $num[$i] = $number['value'];
+  $i++;
+}
+$length = $i;
+
+// echo "<pre>";
+// for ($i = 0; $i <= $length; $i++) {
+//   echo $num[$i] . "<br>";
+// }
+// echo "</pre>";
