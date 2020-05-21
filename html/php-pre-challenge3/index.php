49cd8d70e48a92fc454e1b3731c451df210194f4
nanakusa27 <masakiw27@yahoo.co.jp>
2020-05-21 15:21:01 +0900

    Improve:変数名をわかりやすく変更

--- html/php-pre-challenge3/index.php
+++ html/php-pre-challenge3/index.php
@@ -21,7 +21,6 @@ try {
 // 値取り出し
 $numbers = $db->query('SELECT value FROM prechallenge3');
 
-
 for ($i = 0; $number = $numbers->fetch(); $i++) {
   // print($number['value']);
   $num[$i] = (int)$number['value'];
@@ -36,12 +35,12 @@ $num_length = count($num);
 
 // 組み合わせ列挙
 $take = 2;
-$comb_length = 0;
+$comb_cnt = 0;
 
 for ($i = 0; $i <= $num_length - $take; $i++) {
     for ($j = $i + 1; $j < $num_length; $j++) {
-        $took[$comb_length] = [$num[$i], $num[$j]];
-        $comb_length++;
+        $combination[$comb_cnt] = [$num[$i], $num[$j]];
+        $comb_cnt++;
     }
 }
 
@@ -50,8 +49,8 @@ $take++;
 for ($i = 0; $i <= $num_length - $take; $i++) {
     for ($j = $i + 1; $j < $num_length; $j++) {
         for ($k = $j + 1; $k < $num_length; $k++) {
-            $took[$comb_length] = [$num[$i], $num[$j], $num[$k]];
-            $comb_length++;
+            $combination[$comb_cnt] = [$num[$i], $num[$j], $num[$k]];
+            $comb_cnt++;
         }
     }
 }
@@ -62,8 +61,8 @@ for ($i = 0; $i <= $num_length - $take; $i++) {
     for ($j = $i + 1; $j < $num_length; $j++) {
         for ($k = $j + 1; $k < $num_length; $k++) {
             for ($l = $k + 1; $l < $num_length; $l++) {
-                $took[$comb_length] = [$num[$i], $num[$j], $num[$k], $num[$l]];
-                $comb_length++;
+                $combination[$comb_cnt] = [$num[$i], $num[$j], $num[$k], $num[$l]];
+                $comb_cnt++;
             }
         }
     }
@@ -76,8 +75,8 @@ for ($i = 0; $i <= $num_length - $take; $i++) {
         for ($k = $j + 1; $k < $num_length; $k++) {
             for ($l = $k + 1; $l < $num_length; $l++) {
                 for ($m = $l + 1; $m < $num_length; $m++) {
-                    $took[$comb_length] = [$num[$i], $num[$j], $num[$k], $num[$l], $num[$m]];
-                    $comb_length++;
+                    $combination[$comb_cnt] = [$num[$i], $num[$j], $num[$k], $num[$l], $num[$m]];
+                    $comb_cnt++;
                 }
             }
         }
@@ -92,8 +91,8 @@ for ($i = 0; $i <= $num_length - $take; $i++) {
             for ($l = $k + 1; $l < $num_length; $l++) {
                 for ($m = $l + 1; $m < $num_length; $m++) {
                     for ($n = $m + 1; $n < $num_length; $n++) {
-                        $took[$comb_length] = [$num[$i], $num[$j], $num[$k], $num[$l], $num[$m], $num[$n]];
-                        $comb_length++;
+                        $combination[$comb_cnt] = [$num[$i], $num[$j], $num[$k], $num[$l], $num[$m], $num[$n]];
+                        $comb_cnt++;
                     }
                 }
             }
@@ -110,8 +109,8 @@ for ($i = 0; $i <= $num_length - $take; $i++) {
                 for ($m = $l + 1; $m < $num_length; $m++) {
                     for ($n = $m + 1; $n < $num_length; $n++) {
                         for ($o = $n + 1; $o < $num_length; $o++) {
-                            $took[$comb_length] = [$num[$i], $num[$j], $num[$k], $num[$l], $num[$m], $num[$n], $num[$o]];
-                            $comb_length++;
+                            $combination[$comb_cnt] = [$num[$i], $num[$j], $num[$k], $num[$l], $num[$m], $num[$n], $num[$o]];
+                            $comb_cnt++;
                         }
                     }
                 }
@@ -130,8 +129,8 @@ for ($i = 0; $i <= $num_length - $take; $i++) {
                     for ($n = $m + 1; $n < $num_length; $n++) {
                         for ($o = $n + 1; $o < $num_length; $o++) {
                             for ($p = $o + 1; $p < $num_length; $p++) {
-                                $took[$comb_length] = [$num[$i], $num[$j], $num[$k], $num[$l], $num[$m], $num[$n], $num[$o], $num[$p]];
-                                $comb_length++;
+                                $combination[$comb_cnt] = [$num[$i], $num[$j], $num[$k], $num[$l], $num[$m], $num[$n], $num[$o], $num[$p]];
+                                $comb_cnt++;
                             }
                         }
                     }
@@ -142,16 +141,16 @@ for ($i = 0; $i <= $num_length - $take; $i++) {
 }
 
 // echo"<pre>";
-// print_r($took);
-// echo $comb_length;
+// print_r($combination);
+// echo $comb_cnt;
 // echo"</pre>";
 
 // 組み合わせ照合
 $y = 0;
 settype($limit, "integer");
-for ($x = 0; $x < $comb_length; $x++) {
-    if ($limit === array_sum($took[$x])) {
-        $hit_comb[$y] = $took[$x];
+for ($x = 0; $x < $comb_cnt; $x++) {
+    if ($limit === array_sum($combination[$x])) {
+        $hit_comb[$y] = $combination[$x];
         $y++;
     }
 }
