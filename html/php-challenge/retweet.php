77a94d591a604d5809fd52eb085132b4b05c0e65
nanakusa27 <masakiw27@yahoo.co.jp>
2020-05-26 16:53:14 +0900

    Add:リツイート表示変更動作ファイル

---
+++ html/php-challenge/retweet.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+require('dbconnect.php');
+
+if (isset($_SESSION['id'])) {
+    $id = $_REQUEST['id'];
+
+    $posts = $db->prepare('SELECT * FROM posts WHERE id=?');
+    $posts->execute(array($id));
+    $post = $posts->fetch();
+
+    $rt = unserialize($post['retweets']);
+
+    // 現在の状況を検査する
+    if (is_array($rt)) {
+        for ($i = 0; $i < count($rt); $i++) {
+            if ($_SESSION['id'] == $rt[$i]) {
+                $hit = 1;
+                break;
+            }
+        }
+    }
+    if ($hit) {
+        // retweets(posts)からmember_idを削除
+        unset($rt[$i]);
+    } else {
+        // retweets(posts)にmember_idを追加する
+        $rt[] = $_SESSION['id'];
+    }
+
+    $retweets = serialize($rt);
+    $update = $db->prepare('UPDATE posts SET retweets=? WHERE id=?');
+    $update->execute(array($retweets, $id));
+
+
+}
+
+header('Location: index.php'); exit();
