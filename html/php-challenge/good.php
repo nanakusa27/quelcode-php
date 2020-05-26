e50fb40e5f872ee8d2a272a7084ef0fa31d95271
nanakusa27 <masakiw27@yahoo.co.jp>
2020-05-26 15:48:45 +0900

    Add:いいねボタン押したときの挙動ファイル

---
+++ html/php-challenge/good.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+require('dbconnect.php');
+
+if (isset($_SESSION['id'])) {
+    $id = $_REQUEST['id'];
+
+    $posts = $db->prepare('SELECT * FROM posts WHERE id=?');
+    $posts->execute(array($id));
+    $post = $posts->fetch();
+
+    $gd = unserialize($post['goods']);
+
+    // 現在の状況を検査する
+    for ($i = 0; $i < count($gd); $i++) {
+        if ($_SESSION['id'] == $gd[$i]) {
+            $hit = 1;
+            break;
+        }
+    }
+
+    if ($hit) {
+        // goods(posts)からmember_idを削除
+        unset($gd[$i]);
+    } else {
+        // goods(posts)にmember_idを追加する
+        $gd[] = $_SESSION['id'];
+    }
+
+    $goods = serialize($gd);
+    $update = $db->prepare('UPDATE posts SET goods=? WHERE id=?');
+    $update->execute(array($goods, $id));
+
+
+}
+
+header('Location: index.php'); exit();
